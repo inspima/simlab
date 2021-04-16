e030bd05a71dbc7b46187d29928726230f804a1e
Nambi Sembilu <nambisembilu@gmail.com>
2021-04-16 13:16:43 +0700

    Update > Halaman SInkronisasi

--- protected/modules/master/controllers/Pasien_sinkronController.php
+++ protected/modules/master/controllers/Pasien_sinkronController.php
@@ -1,118 +1,121 @@
 <?php
 
-class Pasien_sinkronController extends Controller
-{
-
-    /**
-     * @return array action filters
-     */
-    public function filters()
+    class Pasien_sinkronController extends Controller
     {
-        return array(
-            'accessControl', // perform access control for CRUD operations
-        );
-    }
 
-    /**
-     * Specifies the access control rules.
-     * This method is used by the 'accessControl' filter.
-     * @return array access control rules
-     */
-    public function accessRules()
-    {
-        return array(
-            array(
-                'allow', // allow authenticated user to perform 'create' and 'update' actions
-                'actions' => array('read', 'readAjax', 'view', 'create', 'update', 'delete', 'excel', 'checkRegistrasi'),
-                'users' => array('@'),
-            ),
-            array(
-                'deny', // deny all users
-                'users' => array('*'),
-            ),
-        );
-    }
-
-    /**
-     * Declares class-based actions.
-     */
-    public function actionRead()
-    {
-        $mode = Yii::app()->request->getPost('mode');
-
-        $tgl_awal = Yii::app()->request->getPost('awal');
-
-        if ($mode == 'sinkron') {
-            $id = Yii::app()->request->getPost('id');
-            $data = $this->GetData($id);
-
-            $pasien = new Pasien;
-            $pasien->nama = $data['name'];
-            $pasien->nik = $data['id_number'];
-            $pasien->test_loop = $data['test_loop'];
-            $pasien->jenis_kelamin = $data['gender'] == 'Laki-Laki' ? '1' : '2';
-            $pasien->tgl_lahir = $data['born_date'];
-            $pasien->umur = $data['age'];
-            $pasien->alamat = $data['address'];
-            $pasien->telephone = $data['phone'];
-            $pasien->hp = $data['mobile'];
-            if ($pasien->save()) {
-                // Insert Tabel Sinkron
-                Yii::app()->db->createCommand()
-                    ->insert(
-                        'sync_antrian',
-                        array(
-                            'id_pasien' => $pasien->id_pasien,
-                            'id_antrian_reg_pasien' => $data['id'],
-                        )
-                    );
-                Yii::app()->user->setFlash('success', 'Data Pasien berhasil disimpan');
-            } else {
-                print_r($pasien->getErrors());
-            }
+        /**
+         * @return array action filters
+         */
+        public function filters()
+        {
+            return array(
+                'accessControl', // perform access control for CRUD operations
+            );
         }
 
-        $query_view = "
+        /**
+         * Specifies the access control rules.
+         * This method is used by the 'accessControl' filter.
+         * @return array access control rules
+         */
+        public function accessRules()
+        {
+            return array(
+                array(
+                    'allow', // allow authenticated user to perform 'create' and 'update' actions
+                    'actions' => array('read', 'readAjax', 'view', 'create', 'update', 'delete', 'excel', 'checkRegistrasi'),
+                    'users' => array('@'),
+                ),
+                array(
+                    'deny', // deny all users
+                    'users' => array('*'),
+                ),
+            );
+        }
+
+        /**
+         * Declares class-based actions.
+         */
+        public function actionRead()
+        {
+            $mode = Yii::app()->request->getParam('mode');
+
+            $sinkron = Yii::app()->request->getParam('sinkron');
+            if (empty($sinkron)) {
+                $sinkron = 0;
+            }
+
+            if ($mode == 'sinkron') {
+                $id = Yii::app()->request->getPost('id');
+                $data = $this->GetData($id);
+
+                $pasien = new Pasien;
+                $pasien->nama = $data['name'];
+                $pasien->nik = $data['id_number'];
+                $pasien->test_loop = $data['test_loop'];
+                $pasien->jenis_kelamin = $data['gender'] == 'Laki-Laki' ? '1' : '2';
+                $pasien->tgl_lahir = $data['born_date'];
+                $pasien->umur = $data['age'];
+                $pasien->alamat = $data['address'];
+                $pasien->telephone = $data['phone'];
+                $pasien->hp = $data['mobile'];
+                if ($pasien->save()) {
+                    // Insert Tabel Sinkron
+                    Yii::app()->db->createCommand()
+                        ->insert(
+                            'sync_antrian',
+                            array(
+                                'id_pasien' => $pasien->id_pasien,
+                                'id_antrian_reg_pasien' => $data['id'],
+                            )
+                        );
+                    Yii::app()->user->setFlash('success', 'Data Pasien berhasil disimpan');
+                } else {
+                    print_r($pasien->getErrors());
+                }
+            }
+
+
+            $query_view = "
             SELECT p.*,k.nama_kota,ag.nama_agama
             FROM pasien p 
             LEFT JOIN kota k ON k.id_kota=p.id_kota_lahir
             LEFT JOIN agama ag ON ag.id_agama=p.id_agama
             ORDER BY p.nama
             ";
-        $query_Sinkron = "
+            $query_Sinkron = "
             SELECT a.*,b.code,c.type,c.name org_name
             FROM registration_patients a
             JOIN registrations b ON a.registration_id = b.id
             JOIN organizations c on c.id=b.organization_id
-            WHERE DATE_FORMAT(b.date, '%Y-%m-%d') <= '$tgl_awal'
             AND b.status>=1
             ORDER BY a.id desc";
-        $pasien = Yii::app()->db->createCommand($query_view)->queryAll();
-        $pasien_sinkron = Yii::app()->db2->createCommand($query_Sinkron)->queryAll();
-        $this->render('read', array(
-            'data_pasien' => $pasien,
-            'pasien_sinkron' => $pasien_sinkron,
-            'awal' => $tgl_awal
-        ));
-    }
+            $pasien = Yii::app()->db->createCommand($query_view)->queryAll();
+            $pasien_sinkron = Yii::app()->db2->createCommand($query_Sinkron)->queryAll();
+            $this->render('read', array(
+                'data_pasien' => $pasien,
+                'pasien_sinkron' => $pasien_sinkron,
+                'sinkron' => $sinkron,
+            ));
+        }
 
-    public function GetData($id)
-    {
+        public function GetData($id)
+        {
 
-        $query = "SELECT * 
+            $query = "SELECT * 
             FROM registration_patients
             WHERE id='{$id}'";
-        $data = Yii::app()->db2->createCommand($query)->queryRow();
-        return $data;
-    }
+            $data = Yii::app()->db2->createCommand($query)->queryRow();
+            return $data;
+        }
 
 
-    public function cekData($id)
-    {
-        $query = "SELECT count(*) 
+        public function cekData($id)
+        {
+            $query = "SELECT count(*) 
             FROM sync_antrian
             WHERE id_antrian_reg_pasien='$id'";
-        $data = Yii::app()->db->createCommand($query)->queryScalar();
-        return $data;
+            $data = Yii::app()->db->createCommand($query)->queryScalar();
+            return $data;
+        }
     }
-}
--- protected/modules/master/views/pasien_sinkron/read.php
+++ protected/modules/master/views/pasien_sinkron/read.php
@@ -12,17 +12,19 @@
             </div> <!-- /widget-header -->
             <div class="widget-content">
                 <h2>Data Antrian Pasien</h2>
-                <form class="form-horizontal form-validation" method="post" style="width: 90%">
-                    <table class="table table-bordered" style="width:35%">
+                <form class="form-horizontal form-validation" method="get" style="width: 98%">
+                    <table class="table table-condensed" style="width:100%">
                         <tr>
-                            <td>Tanggal Registrasi</td>
-                            <td><input type="text" class="span2 datepicker validate[required]" name="awal" value="<?php if ($awal == null) {
-                                    echo date('Y-m-d');
-                                } else {
-                                    echo $awal;
-                                } ?>"></td>
-                            <td>
-                                <button type="submit" class="btn btn-primary">Submit</button>
+                            <td style="text-align: center">
+                                <b>Status Sinkronisasi</b>
+                                <br/>
+                                <select style="margin-top: 10px" name="sinkron">
+                                    <option value="0" <?=$sinkron=='0'?'selected':''?>>Belum</option>
+                                    <option value="1" <?=$sinkron=='1'?'selected':''?>>Sudah</option>
+                                </select>
+                                <hr/>
+                                <button type="submit" class="btn btn-primary">Tampilkan</button>
+                                <hr/>
                             </td>
                         </tr>
                     </table>
@@ -62,47 +64,98 @@
                     <tbody>
                     <?php
                         foreach ($pasien_sinkron as $d):
-                            ?>
-                            <tr>
-                                <td><?php echo $d['code'] ?></td>
-                                <td><?php echo $d['type'] == 'Individu' ? 'Individu' : $d['org_name'] ?></td>
-                                <td><?php echo $d['name'] ?></td>
-                                <td><?php echo $d['id_number'] ?></td>
-                                <td><?php echo $d['test_loop'] ?></td>
-                                <td><?php echo date('d-m-Y', strtotime($d['born_date'])) . " (" . $d['age'] . ")"; ?></td>
-                                <td><?php if ($d['gender'] == 1 || $d['gender'] == 'Laki-Laki') {
-                                        echo "Laki - Laki";
-                                    } else if ($d['gender'] == 2 || $d['gender'] == 'Perempuan') {
-                                        echo "Perempuan";
-                                    } else {
-                                        echo "NOT SET";
-                                    } ?></td>
-                                <td><?php echo $d['address'] ?></td>
-                                <td><?php echo $d['phone'] . ", " . $d['mobile'] ?></td>
-                                <td style="width: 120px;text-align: center">
-
-                                    <p>
-                                        <?php if ($this->cekData($d['id']) >= 1) {
-                                            echo '<div class="btn btn-success" ><b>Registered</b></div>';
-                                        } ?>
-                                    </p>
-                                </td>
-                                <td>
-                                    <?php
-                                        if ($this->cekData($d['id']) == 0) {
-                                            ?>
-                                            <form method="post" action="">
-                                                <button type="submit" class="btn btn-primary">Sinkron</button>
-                                                <input type="hidden" name="mode" value="sinkron"/>
-                                                <input type="hidden" name="id" value="<?= $d['id'] ?>"/>
-                                                <input type="hidden" name="awal" value="<?= $awal ?>"/>
-                                            </form>
-                                            <?php
-                                        }
+                            $status_sinkron = $this->cekData($d['id']) >= 1 ? true : false;
+                            if ($sinkron == '0') {
+                                if (!$status_sinkron) {
                                     ?>
+                                    <tr>
+                                        <td><?php echo $d['code'] ?></td>
+                                        <td><?php echo $d['type'] == 'Individu' ? 'Individu' : $d['org_name'] ?></td>
+                                        <td><?php echo $d['name'] ?></td>
+                                        <td><?php echo $d['id_number'] ?></td>
+                                        <td><?php echo $d['test_loop'] ?></td>
+                                        <td><?php echo date('d-m-Y', strtotime($d['born_date'])) . " (" . $d['age'] . ")"; ?></td>
+                                        <td><?php if ($d['gender'] == 1 || $d['gender'] == 'Laki-Laki') {
+                                                echo "Laki - Laki";
+                                            } else if ($d['gender'] == 2 || $d['gender'] == 'Perempuan') {
+                                                echo "Perempuan";
+                                            } else {
+                                                echo "NOT SET";
+                                            } ?></td>
+                                        <td><?php echo $d['address'] ?></td>
+                                        <td><?php echo $d['phone'] . ", " . $d['mobile'] ?></td>
+                                        <td style="width: 120px;text-align: center">
 
-                                </td>
-                            </tr>
+                                            <p>
+                                                <?php if ($status_sinkron) {
+                                                    echo '<div class="btn btn-success" ><b>Registered</b></div>';
+                                                } ?>
+                                            </p>
+                                        </td>
+                                        <td>
+                                            <?php
+                                                if (!$status_sinkron) {
+                                                    ?>
+                                                    <form method="post" action="">
+                                                        <button type="submit" class="btn btn-primary">Sinkron</button>
+                                                        <input type="hidden" name="mode" value="sinkron"/>
+                                                        <input type="hidden" name="id" value="<?= $d['id'] ?>"/>
+                                                    </form>
+                                                    <?php
+                                                }
+                                            ?>
+
+                                        </td>
+                                    </tr>
+                                    <?php
+                                }
+                            } else {
+                                if ($status_sinkron) {
+                                    ?>
+                                    <tr>
+                                        <td><?php echo $d['code'] ?></td>
+                                        <td><?php echo $d['type'] == 'Individu' ? 'Individu' : $d['org_name'] ?></td>
+                                        <td><?php echo $d['name'] ?></td>
+                                        <td><?php echo $d['id_number'] ?></td>
+                                        <td><?php echo $d['test_loop'] ?></td>
+                                        <td><?php echo date('d-m-Y', strtotime($d['born_date'])) . " (" . $d['age'] . ")"; ?></td>
+                                        <td><?php if ($d['gender'] == 1 || $d['gender'] == 'Laki-Laki') {
+                                                echo "Laki - Laki";
+                                            } else if ($d['gender'] == 2 || $d['gender'] == 'Perempuan') {
+                                                echo "Perempuan";
+                                            } else {
+                                                echo "NOT SET";
+                                            } ?></td>
+                                        <td><?php echo $d['address'] ?></td>
+                                        <td><?php echo $d['phone'] . ", " . $d['mobile'] ?></td>
+                                        <td style="width: 120px;text-align: center">
+
+                                            <p>
+                                                <?php if ($status_sinkron) {
+                                                    echo '<div class="btn btn-success" ><b>Registered</b></div>';
+                                                } ?>
+                                            </p>
+                                        </td>
+                                        <td>
+                                            <?php
+                                                if (!$status_sinkron) {
+                                                    ?>
+                                                    <form method="post" action="">
+                                                        <button type="submit" class="btn btn-primary">Sinkron</button>
+                                                        <input type="hidden" name="mode" value="sinkron"/>
+                                                        <input type="hidden" name="id" value="<?= $d['id'] ?>"/>
+                                                    </form>
+                                                    <?php
+                                                }
+                                            ?>
+
+                                        </td>
+                                    </tr>
+                                    <?php
+                                }
+                            }
+                            ?>
+
                         <?php
                         endforeach;
                     ?>
